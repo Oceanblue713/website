88ee04f969e85091f76cd6f435f96294fe7f0f3d
Anri Liy <amedamachip713@gmail.com>
2021-09-19 18:54:38 -0700

    Start Navbar again

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact />
         </Switch>
       </Router>
     </>
